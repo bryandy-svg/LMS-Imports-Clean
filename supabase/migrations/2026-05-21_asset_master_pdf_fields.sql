d71e34b45d67875d9c487bf2bbbb413bbf79db1c
bryandy-svg <bryan.dy@lmsfm.com>
2026-05-22 17:37:46 +1000

    Delete supabase directory

--- supabase/migrations/2026-05-21_asset_master_pdf_fields.sql
+++
@@ -1,5 +0,0 @@
-alter table assets
-  add column if not exists general_type text,
-  add column if not exists old_qr_code text,
-  add column if not exists last_update_date date,
-  add column if not exists qr_printed_tagged text;
